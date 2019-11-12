e0cb99f1ee4180266a7073f0bc63189d1a8dbccf
Nazaret <nazaretmiranda@gmail.com>
2019-11-12 10:27:38 +0000

    created new reusable button, created custom input for cleaner forms components

---
+++ src/app/shared/buttons/text-square-button/text-square-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-square-button',
+  templateUrl: './text-square-button.component.html',
+  styleUrls: ['./text-square-button.component.scss']
+})
+export class TextSquareButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,16 +12,16 @@ import { StockEntryFormComponent } from './stock-entry-form/stock-entry-form.com
 import { ErrorToastComponent } from './error-toast/error-toast.component';
 import { LoadingToastComponent } from './loading-toast/loading-toast.component';
 import { TestFormComponent } from './custom-forms/test-form/test-form.component';
-import { CustomInputComponent } from './custom-forms/custom-input/custom-input.component';
 import { ElementPickerComponent } from './custom-forms/element-picker/element-picker.component';
 import { QuantityInputComponent } from './custom-forms/quantity-input/quantity-input.component';
 import { ElementFormComponent } from './custom-forms/element-picker/element-form/element-form.component';
 import { StockEntryDataComponent } from './stock-entry-data/stock-entry-data.component';
 import { FloatingButtonPlusComponent } from './buttons/floating-button-plus/floating-button-plus.component';
-import { ActionButtonComponent } from './buttons/action-button/action-button.component';
 import { RoundIconButtonComponent } from './buttons/round-icon-button/round-icon-button.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { TextButtonComponent } from './buttons/text-button/text-button.component';
+import { TextSquareButtonComponent } from './buttons/text-square-button/text-square-button.component';
+import { CustomInputComponent } from './custom-forms/custom-input/custom-input.component';
 
 
 @NgModule({
@@ -30,16 +30,16 @@ import { TextButtonComponent } from './buttons/text-button/text-button.component
     StockEntryFormComponent,
     ErrorToastComponent,
     LoadingToastComponent,
-    CustomInputComponent,
     TestFormComponent,
     ElementPickerComponent,
     QuantityInputComponent,
     ElementFormComponent,
     StockEntryDataComponent,
     FloatingButtonPlusComponent,
-    ActionButtonComponent,
     RoundIconButtonComponent,
-    TextButtonComponent],
+    TextButtonComponent,
+    TextSquareButtonComponent,
+    CustomInputComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -57,10 +57,10 @@ import { TextButtonComponent } from './buttons/text-button/text-button.component
     StockEntryDataComponent,
     TestFormComponent,
     FloatingButtonPlusComponent,
-    ActionButtonComponent,
     QuantityInputComponent,
     RoundIconButtonComponent,
-    TextButtonComponent
+    TextButtonComponent,
+    CustomInputComponent
   ]
 })
 export class SharedModule { }
